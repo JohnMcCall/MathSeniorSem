e1e6536d0e45fcdae9516d90870476ccf94b81da
John McCall <mcca0798@morris.umn.edu>
2014-04-12 02:55:35 -0500

    I still can't get my example to work :-(

--- Ruby Scripts/Main.rb
+++ Ruby Scripts/Main.rb
@@ -15,81 +15,80 @@ ca = CoefficientArithmetic.new
 vc = encoder.getValidCharacters
 replacements = ca.getReplacements
 
-=begin
-rNeg1 = MyPolynomial[6=>'!']
-r0 = MyPolynomial[']', ']', ' ', '7', ' ', '1']
-r1 = MyPolynomial[' ', '+', '+', ' ', 'S']
-r2 = MyPolynomial[']', ']', 'X', 'O']
-puts r1.to_s
-puts r2.to_s
-q, r = r1.divmod(r2)
-puts q
-puts r
-puts ((q * r2) + r).to_s
+#puts ca.findInverse('%')
+#puts ca.findInverse('@')
+#puts ca.findInverse('Z')
 
-puts (MyPolynomial[' ', 'V'] + (MyPolynomial['Z', 'J'] * MyPolynomial['!', '^'])).to_s
-=end
+def euclid(r0, r1)
+    s = [MyPolynomial['!'], MyPolynomial[' ']]
+    t = [MyPolynomial[' '],MyPolynomial['!']]
+    r = [r0, r1]
+    q = ['', '']
 
+    step = 2
+    while (r[step - 1].degree() >= 3) do
+        qNew, rNew = r[step-2].divmod(r[step-1])
+        r.push(rNew)
+        q.push(qNew)
+        
+        sNew = (s[step-2] + (q[step] * s[step-1]))
+        s.push(sNew)
+        
+        tNew = (t[step-2] + (q[step] * t[step-1]))
+        t.push(tNew)
+        
+        step = step + 1
+    end
+    
+    puts s.to_s
+    puts t.to_s
+    puts r.to_s
+    puts q.to_s
+    puts
 
-# The first step it to find a primitive element. 
-# Using the isPrimitive method, we can test many different 
-# elements quickly. Here, I found that $ is a primitive element.
-puts "$ is a primitive element: " + ca.isPrimitive('$').to_s
-puts
-
-
-# Next we create a generater polynomial using the primitive element.
-# For our example, we will have n - k = 17 - 11 = 6 roots.
-roots = 6
-generatorPoly = MyPolynomial["!"]
-(1..roots).each do |n|
-    generatorPoly = MyPolynomial[ca.exp('$', n), '!'] * generatorPoly
 end
-puts "g(x) = " + generatorPoly.to_s
-puts
+
+
+sigma = MyPolynomial['!', ' ', '^', '4']
+vc.each do |elt|
+    if (sigma.substitute(elt) == ' ')
+        puts elt
+    end
+end
+
 
 
 # Now we can create our message polynomial.
 message = "THIS IS MAJOR TOM"
 messagePoly = MyPolynomial.new(message.split(//))
-puts "m(x) = " + messagePoly.to_s
+puts "f(x) = " + messagePoly.to_s
 puts
 
-
-# The next thing we need to do is upshift the degree of our message polynomial.
-# We do this buy multiplying the polynomial by x^(n-k) = x^6
-upshiftedMessage = MyPolynomial[6 => '!'] * messagePoly
-puts "x^6 * m(x) = " + upshiftedMessage.to_s
-puts
-
-
-# Now we need to find the parity bits. The equation for finding the
-# partity bits, p(x), is: p(x) = (x^(n-k) * m(x)) mod g(x)
-parityPoly = upshiftedMessage % generatorPoly
-puts "p(x) = " + parityPoly.to_s
-puts
-
-
-# Then, we add the parity bits to the upshifted message to get our
-# codeword polynomial, f(x).
-codewordPoly = parityPoly + upshiftedMessage
-puts "f(x) = " + codewordPoly.to_s
-puts
-
-
-# Now that we have f(x), we can use it to calculate a codeword, C.
+# Now that we have f(x), we can use it to calculate a codeword, c.
 # For this example C = GRS(a,v), where v = (1,1...1).
-# We want to find a vector u for the dual code of C = GRS(a, u).
-# Note: This calculation took a really long time. So I hard coded in the
-# value for u so I don't have to sit through the calculation everytime.
-a = ('A'..'Q').to_a
+a = ('A'..'W').to_a
 puts "a = " + a.to_s
 puts
+
+c = []
+a.each do |elt|
+    c.push(messagePoly.substitute(elt))
+end
+puts "c = " + c.to_s
+puts
+
+# We want to find a vector u for the dual code of C = GRS(a, u).
+# Note: This calculation took a really long time. So I hard coded in the
+# value for l and u so I don't have to sit through the calculation everytime.
+l = MyPolynomial.new([".", "/", "S", "'", "9", "X", "<", "I", "X", "Y", "^", "5", "@", "!", "_", "J", ";", "(", ")", ".", "H", "P", "@", "!"])
 =begin
 l = '!'
 a.each do |elt|
     l = MyPolynomial[elt, '!'] * l
 end
+
+puts "l(x) coefs: " + l.coefs.to_a.to_s
+puts
 
 u = []
 a.each do |elt|
@@ -98,16 +97,20 @@ a.each do |elt|
 end
 =end
 
-u = [":", "X", "D", "*", "I", "^", "*", "'", "N", "(", "B", "3", "?", "X", "\\", "J", "P"]
+u = ["2", "D", "V", "+", "9", "O", "]", "G", "*", "^", "3", "5", "X", ",", "A", "A", ">", "<", "C", "8", "G", "E", ":"]
 
 puts "u = " + u.to_s
 puts
 
-# Lets say that the message we recieved is "THIS IS MAYOR TED".
+# Lets say that the message we recieved is different from c in 3 places.
+# c[6] was a "G", is now a "Z"
+# c[10] was a "4" is now a "&"
+# c[21] was a "<" is now a "R"
 # we calculate the Syndrome Polynomial for the received message vector, p.
-p = "THIS IS MAYOR TED".split(//)
+p = ["T", "(", "U", "8", "P", "K", "Z", "N", "W", "P", "4", "K", "'", "_", "N", "(", "M", "H", "K", "1", "\"", "<", "K"]
 puts "p = " + p.to_s
-puts 
+puts
+
 
 ## First generate the ui * pi and add them to an array
 products = []
@@ -130,9 +133,10 @@ sp = MyPolynomial[' ']
     sp = sp + mod6
 end
 
-
-#sp = MyPolynomial[']', ']', ' ', '7', ' ', '1']
-puts sp.to_s
+#sp = MyPolynomial['?', "'", '\\', 'I', '4', 'Z']
+puts "Sp = " + sp.to_s
 puts
 
+#Euclids Algorithm
+euclid(MyPolynomial[6 => "!"], sp)
 
---
+++ Ruby Scripts/Main2.rb
@@ -0,0 +1,138 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "polynomial"
+require "./Encoder"
+require "./CoefficientArithmetic.rb"
+require "./MyPolynomial.rb"
+
+
+# Here we create instances of Encoder and CoefficientArithmetic.
+# These will be used in computations to come.
+encoder = Encoder.new
+ca = CoefficientArithmetic.new
+
+vc = encoder.getValidCharacters
+replacements = ca.getReplacements
+
+=begin
+rNeg1 = MyPolynomial[6=>'!']
+r0 = MyPolynomial[']', ']', ' ', '7', ' ', '1']
+r1 = MyPolynomial[' ', '+', '+', ' ', 'S']
+r2 = MyPolynomial[']', ']', 'X', 'O']
+puts r1.to_s
+puts r2.to_s
+q, r = r1.divmod(r2)
+puts q
+puts r
+puts ((q * r2) + r).to_s
+
+puts (MyPolynomial[' ', 'V'] + (MyPolynomial['Z', 'J'] * MyPolynomial['!', '^'])).to_s
+
+
+
+# The first step it to find a primitive element. 
+# Using the isPrimitive method, we can test many different 
+# elements quickly. Here, I found that $ is a primitive element.
+puts "$ is a primitive element: " + ca.isPrimitive('$').to_s
+puts
+
+
+# Next we create a generater polynomial using the primitive element.
+# For our example, we will have n - k = 17 - 11 = 6 roots.
+roots = 6
+generatorPoly = MyPolynomial["!"]
+(1..roots).each do |n|
+    generatorPoly = MyPolynomial[ca.exp('$', n), '!'] * generatorPoly
+end
+puts "g(x) = " + generatorPoly.to_s
+puts
+=end
+
+# Now we can create our message polynomial.
+message = "THIS IS MAJOR TOM"
+messagePoly = MyPolynomial.new(message.split(//))
+puts "f(x) = " + messagePoly.to_s
+puts
+
+=begin
+# The next thing we need to do is upshift the degree of our message polynomial.
+# We do this buy multiplying the polynomial by x^(n-k) = x^6
+upshiftedMessage = MyPolynomial[6 => '!'] * messagePoly
+puts "x^6 * m(x) = " + upshiftedMessage.to_s
+puts
+
+
+# Now we need to find the parity bits. The equation for finding the
+# partity bits, p(x), is: p(x) = (x^(n-k) * m(x)) mod g(x)
+parityPoly = upshiftedMessage % generatorPoly
+puts "p(x) = " + parityPoly.to_s
+puts
+=end
+
+# Then, we add the parity bits to the upshifted message to get our
+# codeword polynomial, f(x).
+codewordPoly = parityPoly + upshiftedMessage
+puts "f(x) = " + codewordPoly.to_s
+puts
+
+
+# Now that we have f(x), we can use it to calculate a codeword, C.
+# For this example C = GRS(a,v), where v = (1,1...1).
+# We want to find a vector u for the dual code of C = GRS(a, u).
+# Note: This calculation took a really long time. So I hard coded in the
+# value for u so I don't have to sit through the calculation everytime.
+a = ('A'..'Q').to_a
+puts "a = " + a.to_s
+puts
+=begin
+l = '!'
+a.each do |elt|
+    l = MyPolynomial[elt, '!'] * l
+end
+
+u = []
+a.each do |elt|
+    result = (l / MyPolynomial[elt, '!']).substitute(elt)
+    u.push(ca.findInverse(result))
+end
+=end
+
+u = [":", "X", "D", "*", "I", "^", "*", "'", "N", "(", "B", "3", "?", "X", "\\", "J", "P"]
+
+puts "u = " + u.to_s
+puts
+
+# Lets say that the message we recieved is "THIS IS MAYOR TED".
+# we calculate the Syndrome Polynomial for the received message vector, p.
+p = "THIS IS MAYOR TED".split(//)
+puts "p = " + p.to_s
+puts 
+
+## First generate the ui * pi and add them to an array
+products = []
+(0...u.length).each do |i|
+    products.push(MyPolynomial[ca.multiply([u[i], p[i]])])
+end
+
+denom = MyPolynomial['!']
+a.each do |elt|
+    denom = denom * MyPolynomial['!', elt]
+end
+
+#  -- Again this took a long time so I ran it once and hardcoded the result into the program
+sp = MyPolynomial[' ']
+(0...u.length).each do |i|
+    mult = denom / MyPolynomial['!', a[i]]
+    # mod by z^6 by taking the first 6 coefs and creating a new poly from them
+    product = products[i] * mult    
+    mod6 = MyPolynomial.new(product.coefs.take(6))
+    sp = sp + mod6
+end
+
+
+#sp = MyPolynomial[']', ']', ' ', '7', ' ', '1']
+puts sp.to_s
+puts
+
+
